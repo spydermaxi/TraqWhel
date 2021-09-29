2ac7a924e921d228a06f99f1ff5e5de48b59b777
Adrian Loo <adrian.loo@axon-bots.com>
2021-09-29 23:36:39 +0800

    Complete StartPage development

--- MW_Tyre_App.py
+++ MW_Tyre_App.py
@@ -14,6 +14,7 @@ The Tyre App for tracking tyre usage and inventory for Modern Wong
 #
 # History
 # 2021-09-29: v0.0.1 [Adrian Loo] Development start
+# 2021-09-29: v0.0.1 [Adrian Loo] StartPage completed
 #
 #-----------------------------------------------------------------------------#
 #                                                                             #
@@ -21,3 +22,272 @@ The Tyre App for tracking tyre usage and inventory for Modern Wong
 #                           All rights reserved                               #
 #                                                                             #
 #-----------------------------------------------------------------------------#
+
+# Import Modules
+
+import glob
+import os
+import sys
+import subprocess
+import re
+import shutil
+import random
+import logging
+import zipfile
+from datetime import datetime
+from distutils.dir_util import copy_tree
+from xml.etree import ElementTree as ET
+from xml.dom import minidom
+
+import pandas as pd
+
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog as tkFileDialog
+from tkinter import messagebox as tkMessageBox
+from tkinter import simpledialog as tkSimpleDialog
+
+# ----- Methods ----- #
+
+LARGE_FONT = ("Verdana", 12, "bold")
+LB_FONT = ("Verdana", 11, "bold")
+RB_FONT = ("Verdana", 10)
+
+
+def create_logger(name, basefile, version, loglevel):
+    '''
+    Method to create logger
+    name: usually refers to the variable __name__
+    basefile: usually refers to the __file__
+    version: usually refers to the __version__
+    loglevel: integer representing the logging level
+        Level       Numeric value
+        CRITICAL    50
+        ERROR       40
+        WARNING     30
+        INFO        20
+        DEBUG       10
+        NOTSET      0
+    '''
+    # Create log directory
+    log_dir = os.path.join(os.getcwd(), "Logs")
+
+    # setup logger
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter(
+        '%(asctime)s\t[%(funcName)s][%(levelname)s]:\t%(message)s')
+
+    file_handler = logging.FileHandler(os.path.join(log_dir, '{}_{}.log'.format(
+        os.path.basename(basefile).split(".")[0], str(datetime.now().date()))))
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(formatter)
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.INFO)
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
+    logger.info("Logging initialized")
+
+    logger.info(
+        "Running from base file - {}, version: {}".format(os.path.basename(basefile), version))
+
+    file_handler.setLevel(loglevel)
+
+    return logger
+
+
+class TyreApp(tk.Tk):
+    '''
+    Main GUI interface for MW Tyre App
+
+    Initiation codes
+    app = ProMonInstaller()
+    app.mainloop()
+    '''
+
+    def __init__(self, *args, **kwargs):
+
+        main_ver = __version__
+
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        tk.Tk.iconbitmap(self, default='Config\\mw_truck.ico')
+        tk.Tk.wm_title(
+            self, "Modern Wong Transport Tyre Inventory and Tracking - {}".format(main_ver))
+
+        self.geometry("+10+10")
+
+        style = ttk.Style(self)
+        style.configure("TRadiobutton", font=RB_FONT)
+        style.configure("TLabelframe.Label", foreground='black', font=LB_FONT)
+
+        logger.info("Application initialized")
+
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+
+        for F in (StartPage, TrackTyrePage, TrackInvPage, DashboardPage):
+
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.start_frame(StartPage)
+
+        self.protocol('WM_DELETE_WINDOW', self.on_exit)
+
+    def start_frame(self, cont):
+
+        frame = self.frames[cont]
+        frame.tkraise()
+        logger.info("Show frame - {}".format(cont))
+        logger.info("Application initialized")
+
+    def show_frame(self, dst_cont):
+
+        frame = self.frames[dst_cont]
+        frame.tkraise()
+        logger.info("Show frame - {}".format(dst_cont))
+
+    def on_exit(self):
+        if tkMessageBox.askyesno('System Warning', 'Do you want to quit the application?'):
+            logger.info("User terminate application")
+            self.destroy()
+            exit()
+        else:
+            pass
+
+
+class StartPage(tk.Frame):
+    '''
+    [Unreleased]
+    The complete installer GUI for ProMon Installation
+    '''
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.configure(height='700', width='1200')
+        self.grid(column='0', row='0', sticky='n')
+
+        self.header_lbl = ttk.Label(self)
+        self.header_lbl.configure(font='{Source Sans Pro} 32 {bold}',
+                                  justify='center', text='Modern Wong Tyre & Inventory Tracking')
+        self.header_lbl.place(anchor='n', x='600', y='100')
+
+        self.track_tyre_lbl = ttk.Label(self)
+        self.track_tyre_lbl.configure(
+            font='{source sans pro} 18 {bold}', justify='center', text='Track\nTyre')
+        self.track_tyre_lbl.place(anchor='n', x='350', y='200')
+
+        self.track_inv_lbl = ttk.Label(self)
+        self.track_inv_lbl.configure(
+            font='{source sans pro} 18 {bold}', justify='center', text='Track\nInventory')
+        self.track_inv_lbl.place(anchor='n', x='600', y='200')
+
+        self.dashboard_lbl = ttk.Label(self)
+        self.dashboard_lbl.configure(
+            font='{source sans pro} 18 {bold}', justify='center', text='Launch\nDashboard')
+        self.dashboard_lbl.place(anchor='n', x='850', y='200')
+
+        self.track_tyre_btn = ttk.Button(
+            self, command=lambda: controller.show_frame(TrackTyrePage))
+        self.mw_blue_truck_png = tk.PhotoImage(
+            file=os.path.join(os.getcwd(), "Config", 'mw_blue_truck.png'))
+        self.track_tyre_btn.configure(
+            image=self.mw_blue_truck_png, text='Track Tyre')
+        self.track_tyre_btn.place(
+            anchor='n', height='140', width='140', x='350', y='300')
+
+        self.track_inv_btn = ttk.Button(
+            self, command=lambda: controller.show_frame(TrackInvPage))
+        self.mw_green_truck_png = tk.PhotoImage(
+            file=os.path.join(os.getcwd(), "Config", 'mw_green_truck.png'))
+        self.track_inv_btn.configure(
+            image=self.mw_green_truck_png, text='Track Inventory')
+        self.track_inv_btn.place(
+            anchor='n', height='140', width='140', x='600', y='300')
+
+        self.launch_db_btn = ttk.Button(
+            self, command=lambda: controller.show_frame(DashboardPage))
+        self.mw_white_truck_png = tk.PhotoImage(
+            file=os.path.join(os.getcwd(), "Config", 'mw_white_truck.png'))
+        self.launch_db_btn.configure(
+            image=self.mw_white_truck_png, text='Launch Dashboard')
+        self.launch_db_btn.place(
+            anchor='n', height='140', width='140', x='850', y='300')
+
+        self.exit_btn = ttk.Button(self, text="Close", width=20, command=lambda: controller.on_exit())
+        self.exit_btn.place(anchor='n', x='600', y='650')
+
+
+class TrackTyrePage(tk.Frame):
+    '''
+    [Unreleased]
+    The complete installer GUI for ProMon Installation
+    '''
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.configure(height='700', width='1200')
+        self.grid(column='0', row='0', sticky='n')
+
+
+
+        self.exit_btn = ttk.Button(self, text="Close", width=20, command=lambda: controller.on_exit())
+        self.exit_btn.place(anchor='n', x='600', y='650')
+
+
+class TrackInvPage(tk.Frame):
+    '''
+    [Unreleased]
+    The complete installer GUI for ProMon Installation
+    '''
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.configure(height='700', width='1200')
+        self.grid(column='0', row='0', sticky='n')
+
+
+
+        self.exit_btn = ttk.Button(self, text="Close", width=20, command=lambda: controller.on_exit())
+        self.exit_btn.place(anchor='n', x='600', y='650')
+
+
+class DashboardPage(tk.Frame):
+    '''
+    [Unreleased]
+    The complete installer GUI for ProMon Installation
+    '''
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.configure(height='700', width='1200')
+        self.grid(column='0', row='0', sticky='n')
+
+
+
+        self.exit_btn = ttk.Button(self, text="Close", width=20, command=lambda: controller.on_exit())
+        self.exit_btn.place(anchor='n', x='600', y='650')
+
+
+# ----- Execution ----- #
+
+
+if __name__ == "__main__":
+
+    # Create logger
+    logger = create_logger(__name__, __file__, __version__, 10)
+
+    # Launch App
+    app = TyreApp()
+    app.mainloop()
