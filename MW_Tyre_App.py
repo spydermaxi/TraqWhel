8043c663dc1860d1d8fd68f7bd6dbdf51e7b6bb5
Adrian Loo <adrian.loo@axon-bots.com>
2021-09-29 13:28:57 +0800

    First Commit

---
+++ MW_Tyre_App.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+#-----------------------------------------------------------------------------#
+#                                                                             #
+#                            Python script                                    #
+#                                                                             #
+#-----------------------------------------------------------------------------#
+#
+# Ident        : ProMon_Setup.py
+__version__ = "0.0.1"
+__author__ = "Adrian Loo"
+"""
+The Tyre App for tracking tyre usage and inventory for Modern Wong
+"""
+#
+# History
+# 2021-09-29: v0.0.1 [Adrian Loo] Development start
+#
+#-----------------------------------------------------------------------------#
+#                                                                             #
+#                  Copyright (c) 2021, AXONBOTS PTE. LTD.                     #
+#                           All rights reserved                               #
+#                                                                             #
+#-----------------------------------------------------------------------------#
